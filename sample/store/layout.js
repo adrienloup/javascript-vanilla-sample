c47cf1f70a083dae6a7ee9839bdbe34f2f15e433
Adrien Loup <adloup@gmail.com>
2019-02-19 21:42:13 +0100

    Add store 🛍️

--- sample/store/layout.js
+++ sample/store/layout.js
@@ -57,13 +57,16 @@
 	for (let i = 0, length = productsList.length; i < length; i++) {
 		products.push(new Product(productsList[i]))
 	}
+	products[14].sales(50)
+	products[15].sales(50)
+	products[16].sales(50)
 	products[17].sales(30)
 	products[18].sales(50)
 	products[19].sales(50)
 
 	var randomProducts = random(products.slice())
 	for (let i = 0, length = randomProducts.length; i < length; i++) {
-		product += '<div id="' + randomProducts[i].id + '" class="product' + (randomProducts[i].discount ? ' soldes' : '') + '"><div class="img"><img src="img/' + randomProducts[i].img + '.jpeg" alt="' + randomProducts[i].title + '"><svg class="bag" width="40" height="40" viewBox="0 0 40 40"><circle cx="20" cy="20" r="20"></circle><path d="M28,26.987 L28,16 L12,16 L12,27 L28,26.987 Z M14.077,14 C14.5638733,11.1096772 17.0689616,8.99496487 20,9 C22.973,9 25.562,11.162 26.038,14 L30,14 L30,28.986 L10,29 L10,14 L14.077,14 Z M23.979,13.995 C23.531,12.275 21.86,11 20,11 C18.153,11 16.604,12.294 16.144,13.995 C17.064,13.995 23.798,14.025 23.979,13.995 Z" stroke-width="1"></path></svg></div><div class="caption"><span class="brand">' + randomProducts[i].brand + ' - </span><span class="title">' + randomProducts[i].title + ' - </span><span class="color">' + randomProducts[i].color[0] + '</span><span class="price">' + randomProducts[i].price + ' €' + (randomProducts[i].discount ? '<span>' + randomProducts[i].discount.toFixed(2) + ' €</span>' : '') + '</span></div></div>'
+		product += '<div id="' + randomProducts[i].id + '" class="product' + (randomProducts[i].discount ? ' soldes' : '') + '"><div class="img"><img src="img/' + randomProducts[i].img + '.jpg" alt="' + randomProducts[i].title + '"><svg class="bag" width="40" height="40" viewBox="0 0 40 40"><circle cx="20" cy="20" r="20"></circle><path d="M28,26.987 L28,16 L12,16 L12,27 L28,26.987 Z M14.077,14 C14.5638733,11.1096772 17.0689616,8.99496487 20,9 C22.973,9 25.562,11.162 26.038,14 L30,14 L30,28.986 L10,29 L10,14 L14.077,14 Z M23.979,13.995 C23.531,12.275 21.86,11 20,11 C18.153,11 16.604,12.294 16.144,13.995 C17.064,13.995 23.798,14.025 23.979,13.995 Z" stroke-width="1"></path></svg></div><div class="caption"><span class="brand">' + randomProducts[i].brand + ' - </span><span class="title">' + randomProducts[i].title + ' - </span><span class="color">' + randomProducts[i].color[0] + '</span><span class="price">' + randomProducts[i].price + ' €' + (randomProducts[i].discount ? '<span>' + randomProducts[i].discount.toFixed(2) + ' €</span>' : '') + '</span></div></div>'
 		document.getElementById('products').innerHTML = product
 	}
 	console.log('Tous → Tous →',products.length)
@@ -125,10 +128,10 @@
 			}
 		}
 		if (_randomProducts === undefined) {
-			_product = ' Aucun produit :('
+			_product = '<div class="empty">No product there :(</div>'
 		} else {
 			for (let l = 0, length = _randomProducts.length; l < length; l++) {
-				_product += '<div id="' + _randomProducts[l].id + '" class="product' + (_randomProducts[l].bag ? ' in-bag' : '') + (_randomProducts[l].discount ? ' soldes' : '') + '"><div class="img"><img src="img/' + _randomProducts[l].img + '.jpeg" alt="' + _randomProducts[l].title + '"><svg class="bag" width="40" height="40" viewBox="0 0 40 40"><circle cx="20" cy="20" r="20"></circle><path d="M28,26.987 L28,16 L12,16 L12,27 L28,26.987 Z M14.077,14 C14.5638733,11.1096772 17.0689616,8.99496487 20,9 C22.973,9 25.562,11.162 26.038,14 L30,14 L30,28.986 L10,29 L10,14 L14.077,14 Z M23.979,13.995 C23.531,12.275 21.86,11 20,11 C18.153,11 16.604,12.294 16.144,13.995 C17.064,13.995 23.798,14.025 23.979,13.995 Z" stroke-width="1"></path></svg></div><div class="caption"><span class="brand">' + _randomProducts[l].brand + ' - </span><span class="title">' + _randomProducts[l].title + ' - </span><span class="color">' + _randomProducts[l].color[0] + '</span><span class="price">' + _randomProducts[l].price + ' €' + (_randomProducts[l].discount ? '<span>' + _randomProducts[l].discount.toFixed(2) + ' €</span>' : '') + '</span></div></div>'
+				_product += '<div id="' + _randomProducts[l].id + '" class="product' + (_randomProducts[l].bag ? ' in-bag' : '') + (_randomProducts[l].discount ? ' soldes' : '') + '"><div class="img"><img src="img/' + _randomProducts[l].img + '.jpg" alt="' + _randomProducts[l].title + '"><svg class="bag" width="40" height="40" viewBox="0 0 40 40"><circle cx="20" cy="20" r="20"></circle><path d="M28,26.987 L28,16 L12,16 L12,27 L28,26.987 Z M14.077,14 C14.5638733,11.1096772 17.0689616,8.99496487 20,9 C22.973,9 25.562,11.162 26.038,14 L30,14 L30,28.986 L10,29 L10,14 L14.077,14 Z M23.979,13.995 C23.531,12.275 21.86,11 20,11 C18.153,11 16.604,12.294 16.144,13.995 C17.064,13.995 23.798,14.025 23.979,13.995 Z" stroke-width="1"></path></svg></div><div class="caption"><span class="brand">' + _randomProducts[l].brand + ' - </span><span class="title">' + _randomProducts[l].title + ' - </span><span class="color">' + _randomProducts[l].color[0] + '</span><span class="price">' + _randomProducts[l].price + ' €' + (_randomProducts[l].discount ? '<span>' + _randomProducts[l].discount.toFixed(2) + ' €</span>' : '') + '</span></div></div>'
 			}
 		}
 		document.getElementById('products').innerHTML = _product
@@ -155,7 +158,7 @@
 				let $product = $products.querySelector('#'+product.id)
 				if (!products[i].bag) {
 					products[i].bag = true
-					let html = '<div id="' + products[i].id + '" class="product' + (products[i].discount ? ' soldes' : '') + '"><div class="img"><img src="img/' + products[i].img + '.jpeg" alt="' + products[i].title + '"></div><div class="caption"><span class="title">' + products[i].title + '</span><span class="price">' + (products[i].discount ? products[i].discount.toFixed(2) : products[i].price) + ' €</span></div></div>'
+					let html = '<div id="' + products[i].id + '" class="product' + (products[i].discount ? ' soldes' : '') + '"><div class="img"><img src="img/' + products[i].img + '.jpg" alt="' + products[i].title + '"></div><div class="caption"><span class="title">' + products[i].title + '</span><span class="price">' + (products[i].discount ? products[i].discount.toFixed(2) : products[i].price) + ' €</span></div></div>'
 					$list.insertAdjacentHTML('beforeend', html)
 					product.classList.add('in-bag')
 					if (!$product.classList.contains('in-bag')) {
@@ -189,7 +192,7 @@
 	function gotoDetails(product) {
 		for (let i = 0; i < products.length; i++) {
 			if (products[i].id == product.id) {
-				let _product = '<div id="' + products[i].id + '" class="product' + (products[i].discount ? ' soldes' : '') + '"><div class="img"><img src="img/' + products[i].img + '.jpeg" alt="' + products[i].title + '"></div><div class="caption"><span class="brand">' + products[i].brand + '</span> - <span class="title">' + products[i].title + '</span><span class="collection">Collection ' + products[i].collection + '</span><span class="color">Couleurs : ' + products[i].color.join(', ') + '</span><span class="size">Tailles : ' + products[i].size.join(', ') + '</span><span class="price">' + products[i].price + ' €' + (products[i].discount ? '<span>' + products[i].discount.toFixed(2) + ' €</span>' : '') + '</span><span class="bag' + (products[i].bag ? ' disable' : '') + '">' + (products[i].bag ? 'Retirer du panier' : 'Ajouter au panier') + '</span></div>'
+				let _product = '<div id="' + products[i].id + '" class="product' + (products[i].discount ? ' soldes' : '') + '"><div class="img"><img src="img/' + products[i].img + '.jpg" alt="' + products[i].title + '"></div><div class="caption"><span class="brand">' + products[i].brand + '</span> - <span class="title">' + products[i].title + '</span><span class="collection">Collection ' + products[i].collection + '</span><span class="color">Colors: ' + products[i].color.join(', ') + '</span><span class="size">Sizes: ' + products[i].size.join(', ') + '</span><span class="price">' + products[i].price + ' €' + (products[i].discount ? '<span>' + products[i].discount.toFixed(2) + ' €</span>' : '') + '</span><span class="bag' + (products[i].bag ? ' disable' : '') + '">' + (products[i].bag ? 'Remove to bag' : 'Add to bag') + '</span></div>'
 				$details.innerHTML = _product
 				$body.classList.add('details')
 				break
